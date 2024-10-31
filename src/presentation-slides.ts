79ed2f75cf7aad4670b15888d4515e1bb11a17ab
Quin Carter <quin.carter@gmail.com>
2024-10-31 11:36:15 -0400

    Added slides for signals and images

--- src/presentation-slides.ts
+++ src/presentation-slides.ts
@@ -13,8 +13,14 @@ import sdk from "@stackblitz/sdk";
  */
 @customElement("presentation-slides")
 export class PresentationSlides extends LitElement {
-  @query("#slide9 > .o-wrapper")
-  slide9?: HTMLElement;
+  @query("#slide25 > .o-wrapper")
+  stackBlitzAngularEmbed?: HTMLElement;
+
+  @query("#slide27 > .o-wrapper")
+  stackBlitzReactEmbed?: HTMLElement;
+
+  @query("#slide29 > .o-wrapper")
+  stackBlitzLitEmbed?: HTMLElement;
 
   protected createRenderRoot(): HTMLElement | DocumentFragment {
     return this;
@@ -25,12 +31,31 @@ export class PresentationSlides extends LitElement {
   ): void {
     super.firstUpdated(_changedProperties);
     this.initSlideNav();
+
     sdk.embedProjectId(
-      this.slide9 as HTMLElement,
+      this.stackBlitzAngularEmbed as HTMLElement,
+      "angular-with-signals",
+      {
+        forceEmbedLayout: true,
+        openFile: "src/counter.ts",
+      }
+    );
+
+    sdk.embedProjectId(
+      this.stackBlitzLitEmbed as HTMLElement,
       "lit-with-complex-preact-signal-state",
       {
         forceEmbedLayout: true,
         openFile: "src/my-element.ts",
+      }
+    );
+
+    sdk.embedProjectId(
+      this.stackBlitzReactEmbed as HTMLElement,
+      "react-signals-counter",
+      {
+        forceEmbedLayout: true,
+        openFile: "src/SignalCounter.tsx",
       }
     );
   }
@@ -155,10 +180,10 @@ export class PresentationSlides extends LitElement {
                 class="c-logo-main"
               />
             </a>
-            <a href="#slide28">
+            <a href="#slide9">
               <img
-                src="src/assets/web-workers.jpg"
-                alt="web workers image"
+                src="src/assets/javascript-signals.png"
+                alt="javascript signals image"
                 class="workers"
               />
             </a>
@@ -264,15 +289,204 @@ export class PresentationSlides extends LitElement {
           />
         </div>
       </section>
+
       <section id="slide9">
+        <div class="o-wrapper">
+          <h1>JavaScript Signals</h1>
+          <img
+            src="src/assets/javascript-signals.png"
+            alt="javascript signals image"
+            class="workers"
+          />
+        </div>
+      </section>
+
+      <section id="slide10">
+        <div class="o-wrapper">
+          <h1>So what are they?</h1>
+        </div>
+      </section>
+
+      <section id="slide11">
+        <div class="o-wrapper">
+          <h1>So what are they?</h1>
+          <p>Signals are data structures for managing observable state.</p>
+        </div>
+      </section>
+
+      <section id="slide12">
+        <div class="o-wrapper">
+          <h1>What Can I Store in Signals?</h1>
+          <ul>
+            <li>Single Value</li>
+            <li>Computed Value</li>
+          </ul>
+        </div>
+      </section>
+
+      <section id="slide13">
+        <div class="o-wrapper">
+          <h1>TC39 Proposal</h1>
+          <img src="" alt="tc39" />
+        </div>
+      </section>
+
+      <section id="slide14">
+        <div class="o-wrapper">
+          <h1>Why should I Care about TC39?</h1>
+          <img
+            src="https://media.tenor.com/5SKA5XuifgoAAAAM/swedish-chef-baking.gif"
+            alt="swedish chef"
+          />
+          <img
+            src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHplYjUwNWk0Yms5NXdqN2p2NzdiMzBqeGN5MzR1eWo0YWU1eTZnNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41YtBXZvSRdgqq7m/giphy.gif"
+            alt="bake off chef"
+          />
+        </div>
+      </section>
+      <section id="slide15">
+        <div class="o-wrapper">
+          <h1>How Do they work?</h1>
+          <p>
+            What is the first thing that comes to mind when you think about a
+            signal?
+          </p>
+        </div>
+      </section>
+      <section id="slide16">
+        <div class="o-wrapper">
+          <h1>Pub/Sub?</h1>
+          <img src="src/assets/signals/pubsub-diagram.png" alt="pub sub" />
+        </div>
+      </section>
+      <section id="slide17">
+        <div class="o-wrapper">
+          <img
+            src="https://media.tenor.com/Bw7zUTWpZtgAAAAM/pissed.gif"
+            alt="john c reilly"
+          />
+          <h1>How do they work, really?</h1>
+          There are only two main units:
+
+          <ol>
+            <li>Publishers — Components that triggered the events.</li>
+            <li>
+              Subscribers — Components that listen for specific signals and
+              react accordingly.
+            </li>
+          </ol>
+
+          <img
+            src="src/assets/signals/amazon-example.png"
+            alt="amazon example"
+          />
+        </div>
+      </section>
+      <section id="slide18">
+        <div class="o-wrapper">
+          <h1>When Should I Use Signals?</h1>
+          <ol>
+            <li>
+              You need direct communication between components without the
+              complexity of an intermediary event bus.
+            </li>
+            <li>
+              Efficiency, modularity, and scalability are priorities in your
+              application architecture.
+            </li>
+          </ol>
+          <img
+            src="https://media.tenor.com/h38lEyHLgcMAAAAM/radio-national-radio-day.gif"
+            alt="radio tune in"
+          />
+        </div>
+      </section>
+
+      <section id="slide19">
+        <div class="o-wrapper">
+          <h1>Basic Implementation Examples</h1>
+          <img
+            src="https://media.tenor.com/drKuhgBblzcAAAAM/anton-hacker.gif"
+            alt="nerd hacking"
+          />
+        </div>
+      </section>
+      <section id="slide20">
+        <div class="o-wrapper">
+          <h1>Creating a new Signal</h1>
+          <img src="src/assets/signals/new-signal.png" alt="new signal" />
+        </div>
+      </section>
+      <section id="slide21">
+        <div class="o-wrapper">
+          <h1>New Computed Signal</h1>
+          <img
+            src="src/assets/signals/new-computed-signal.png"
+            alt="new computed signal"
+          />
+        </div>
+      </section>
+      <section id="slide22">
+        <div class="o-wrapper">
+          <h1>New Watcher</h1>
+          <img src="src/assets/signals/new-watcher.png" alt="new watcher" />
+        </div>
+      </section>
+      <section id="slide23">
+        <div class="o-wrapper">
+          <h1>Getting a computed signal</h1>
+          <img
+            src="src/assets/signals/computed-signal-get.png"
+            alt="getting a computed signal"
+          />
+        </div>
+      </section>
+
+      <section id="slide24">
+        <div class="o-wrapper">
+          <h1>Angular Example</h1>
+          <img
+            src="src/assets/signals/angular-signals.png"
+            alt="angular signals"
+          />
+        </div>
+      </section>
+
+      <section id="slide25">
+        <div class="o-wrapper">
+          <h1>Angular with Signals</h1>
+        </div>
+      </section>
+
+      <section id="slide26">
+        <div class="o-wrapper">
+          <h1>React Example</h1>
+          <img src="src/assets/signals/react-signals.png" alt="react signals" />
+        </div>
+      </section>
+
+      <section id="slide27">
+        <div class="o-wrapper">
+          <h1>React Example</h1>
+        </div>
+      </section>
+
+      <section id="slide28">
+        <div class="o-wrapper">
+          <h1>More Complex - Lit Example</h1>
+          <img src="src/assets/signals/lit-signals.png" alt="lit signals" />
+        </div>
+      </section>
+
+      <section id="slide29">
         <div class="o-wrapper">
           <h1>Lit with Signals</h1>
         </div>
       </section>
 
-      <section id="slide49">
+      <section id="slide30">
         <div class="o-wrapper">
-          <h1>Multi-Threading The Web</h1>
+          <h1>Plugging my other articles:</h1>
           <img
             class="qr-code"
             src="src/assets/qr-code-multi-threading-the-web.png"
@@ -281,7 +495,7 @@ export class PresentationSlides extends LitElement {
         </div>
       </section>
 
-      <section id="slide50">
+      <section id="slide31">
         <div class="o-wrapper">
           <img src="src/assets/qanda.jpeg" alt="Q & A" />
         </div>
